b9ecceb5c0aef616a29d51531199942a47e7a9aa
_imgmaterial <paul.oshchepkov@icloud.com>
2023-12-19 22:28:32 +0100

    Added the file manager class

--- mauModule7/FileManager.cs
+++ mauModule7/FileManager.cs
@@ -1,8 +1,100 @@
 ﻿using System;
+using System.Diagnostics;
 
 public class FileManager
 {
-	public FileManager()
+    private const string fileVersionToken = "ShowTracker_01";
+    private const double fileVersionNr = 1.0;
+
+
+	public bool WriteToTextFile(List<Show> showList,string textFile) 
 	{
-	}
+        bool ok = true;
+        StreamWriter? writer = null;
+        try
+        {
+            int listLength = showList.Count;
+            writer = new StreamWriter(textFile);
+            writer.WriteLine(fileVersionToken);
+            writer.WriteLine(fileVersionNr);
+            writer.WriteLine(listLength);
+            for (int i = 0; i < listLength; i++)
+            {
+                Show show = showList[i];
+                writer.WriteLine(show.Title);
+                writer.WriteLine(show.Type.ToString());
+                writer.WriteLine(show.Status.ToString());
+                writer.WriteLine(show.MaxEpisodes);
+                writer.WriteLine(show.CurrentEpisodes);
+                writer.WriteLine(show.UserScore);
+            }
+        }
+        catch
+        {
+            ok = false;
+        }
+        finally
+        {
+            if (writer != null)
+            {
+                writer.Close();
+            }
+        }
+        return ok;
+    }
+
+    public bool ReadFromTextFile(List<Show> showList, string textFile)
+	{
+        bool ok = true;
+        StreamReader reader = null;
+
+        try
+        {
+            if (showList != null)
+            {
+                showList.Clear();
+            }
+            else
+            {
+                showList = new List<Show>();
+            }
+
+            reader = new StreamReader(textFile);
+            string versionTest = reader.ReadLine();
+            double version = double.Parse(reader.ReadLine());
+            if ((versionTest == fileVersionToken) && (version == fileVersionNr))
+            {
+                int count = int.Parse(reader.ReadLine());
+                for (int i = 0; i < count; i++)
+                {
+                    Show show = new Show();
+                    show.Title = reader.ReadLine();
+                    show.Type = (ShowType)Enum.Parse(typeof(ShowType), reader.ReadLine());
+                    show.Status = (WatchStatus)Enum.Parse(typeof(WatchStatus), reader.ReadLine());
+                    show.MaxEpisodes = int.Parse(reader.ReadLine());
+                    show.CurrentEpisodes = int.Parse(reader.ReadLine());
+                    show.UserScore = double.Parse(reader.ReadLine());
+                    showList.Add(show);
+                }
+            }
+            else
+            {
+                ok = false;
+            }
+        }
+        catch
+        {
+            ok = false;
+        }
+        finally
+        {
+            if (reader != null)
+            {
+                reader.Close();
+            }
+        }
+        return ok;
+    }
+
+	
 }
